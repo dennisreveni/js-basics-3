4dae920644f1642575de71c87c15fd2ab6b49c9d
dennisreveni <dennisreveni@rambler.ru>
2015-02-03 00:03:48 +0200

    Update solution.js
    вроде так.

--- solution.js
+++ solution.js
@@ -1,71 +1,108 @@
 function range(start, end, step) {
-  // Write a range function that takes two arguments, start and end,
-  // and returns an array containing all the numbers from start up to (and including) end.
+	step = step ? step : 1;
+	if ((end-start)*step > 0) {
+    	var rangeArr = [];
+    	for (i=start; i<=end; i+=step){
+        	rangeArr.push(i);
+    	}
+    	return rangeArr;  
+	}else {return false;}                
 }
 
 function sum(numbers) {
-  // Write a sum function that takes an array of numbers
-  // and returns the sum of these numbers.
+    var result = 0;                  
+    for (i=0; i<numbers.length; i++){
+       var arrItem = parseInt(numbers[i]);
+       result += (!isNaN(arrItem)) ? arrItem : 0;
+    }
+    return result;
 }
 
 function reverseArray(arr) {
-  // Write a function which takes an array as argument
-  // and produces a new array that has the same elements in the inverse order.
-}
+    var newArr = [];
+    for (i=1; i<=arr.length; i++){
+        newArr.push(arr[arr.length-i]);
+    }
+    return newArr
+} 
+
 
 function reverseArrayInPlace(arr) {
-  // Write a function that does what the reverse method does:
-  // it modifies the array given as argument in order to reverse
-  // its elements. It should not use the standard reverse method.
+    var tempArray = [];
+    for (i=1; i<=arr.length; i++){
+        tempArray.push(arr[arr.length-i]);
+    }
+	for (i=0; i<tempArray.length; i++){
+		arr[i] = tempArray[i];	
+	}
+	return arr;
 }
 
 function arrayToList(arr) {
-  // Objects, as generic blobs of values, can be used to build all
-  // sorts of data structures. A common data structure is the list
-  // (not to be confused with the array). A list is a nested set of
-  // objects, with the first object holding a reference to the second,
-  // the second to the third, and so on.
-  // For example:
-  //
-  // var list = {
-  //   value: 1,
-  //   rest: {
-  //     value: 2,
-  //     rest: {
-  //       value: 3,
-  //       rest: null
-  //     }
-  //   }
-  // };
-  //
-  // Write a function arrayToList that builds up a data structure like
-  // the previous one when given [1, 2, 3] as argument. It should use
-  // helper function prepend.
+	var list = null;
+	for (i=0; i<arr.length; i++) {
+		list = prepend(arr[i], list);
+	}
+	return list;
 }
 
-function listToArray(list) {
-  // Write a function that produces an array from a list
+ function listToArray(list) {
+	var newArr = [];
+	function toArr(list) {
+		newArr.push(list.value);
+		return (list.rest) ? toArr(list.rest) : newArr
+	}
+	toArr(list);
+	return newArr;
 }
 
 function prepend(item, list) {
-  // Write a function which takes an element and a list and creates a new list
-  // that adds the element to the front of the input list.
+	return {value: item, rest: list};
 }
 
 function nth(n, list) {
-  // Write which takes a list and a number and returns the element at the
-  // given position in the list, or undefined when there is no such element.
-  // It should be recursive.
+	if (n == 0) {
+		return list.value;
+	} else if (list.rest == null) {
+		return undefined;
+	} else {
+		return nth(n - 1, list.rest);
+	}
 }
 
+/*var list = {
+	value: 1,
+	rest: {
+		value: 2,
+		rest: {
+			value: 3,
+			rest: {
+				value: 4,
+				rest: null
+			}
+		}
+	}
+}
+
+console.log(nth(3, list))*/
+
 function deepEqual(a, b) {
-  // The == operator compares objects by identity. But sometimes,
-  // you would prefer to compare the values of their actual properties.
-  //
-  // Write a function, deepEqual, that takes two values and returns true
-  // only if they are the same value or are objects with the same
-  // properties whose values are also equal when compared with
-  // a recursive call to deepEqual.
+	if (a !== null && b !== null && typeof a === 'object' && typeof b === 'object') {
+		if (Object.getOwnPropertyNames(a).length !== Object.getOwnPropertyNames(a).length) {
+			return false;
+		}
+
+		for (i = 0; i < Object.getOwnPropertyNames(a).length; i++) {
+			if (!b.hasOwnProperty(Object.getOwnPropertyNames[i])) {
+				return false;
+			} else if (!deepEqual(a[Object.getOwnPropertyNames(a)[i]], b[Object.getOwnPropertyNames(a)[i]])) {
+				return false;
+			}
+		}
+		return true;
+	} else {
+		return a === b;
+	}
 }
 
 module.exports = {
